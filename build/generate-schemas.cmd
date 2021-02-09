925bbb42576e06bf80f4924e20b5a07d678e710e
Mark Custer <>
2021-02-08 19:37:01 -0500

    first part of updating the build files to also include schematron, etc, but still need to make a few updates when we add EAD to the mix

--- build/generate-schemas.cmd
+++ build/generate-schemas.cmd
@@ -13,19 +13,23 @@ if %errorlevel%==1 (
 )
 
 
-rem still need to update for when we switch between EAD / EAC transformations, which we can do by passing parameters 
+rem still need to update for when we switch between EAD / EAC transformations, which we can do by passing parameters
 
 @echo Getting started.
 
-java %parameters% %CP% net.sf.saxon.Transform -t -xsl:transformations\prep-source-schema-files.xsl -it 
+java %parameters% %CP% net.sf.saxon.Transform -t -xsl:transformations\prep-source-schema-files.xsl -it
 
-java -jar %jing% -s ..\source\modules\do-not-edit-directly\eac\eac-source.rng > ..\xml-schemas\eac-cpf\cpf.rng
+java -jar %jing% -s ..\source\modules\extensible-version\eac\eac-source.rng > ..\xml-schemas\eac-cpf\eac.rng
 
-java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\eac-cpf\cpf.rng -xsl:transformations\add-comments-and-metadata.xsl -o:..\xml-schemas\eac-cpf\cpf.rng -warnings:silent
+java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\eac-cpf\eac.rng -xsl:transformations\add-comments-and-metadata.xsl -o:..\xml-schemas\eac-cpf\eac.rng -warnings:silent
 
-java -jar %trang% -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ..\xml-schemas\eac-cpf\cpf.rng ..\xml-schemas\eac-cpf\cpf.xsd
+java -jar %trang% -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ..\xml-schemas\eac-cpf\eac.rng ..\xml-schemas\eac-cpf\eac.xsd
 
-java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\eac-cpf\cpf.xsd -xsl:transformations\deglobalize-xsd.xsl -o:..\xml-schemas\eac-cpf\cpf.xsd -warnings:silent
+java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\eac-cpf\eac.xsd -xsl:transformations\deglobalize-xsd.xsl -o:..\xml-schemas\eac-cpf\eac.xsd -warnings:silent
+
+rem temporary.  will change this later in case we need to process the schematron file before moving.
+
+copy ..\source\modules\schematron\shared-rules.sch ..\xml-schemas\eac-cpf\schematron\eac.sch
 
 @echo All done.
 
--- build/generate-schemas.sh
+++ build/generate-schemas.sh
@@ -9,14 +9,17 @@ trang="../vendor/trang-20091111/trang.jar"
 
 echo "Getting started."
 
-java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it 
+java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it
 
-java -jar $jing -s ../source/modules/do-not-edit-directly/eac/eac-source.rng > ../xml-schemas/eac-cpf/cpf.rng
+java -jar $jing -s ../source/modules/extensible-version/eac/eac-source.rng > ../xml-schemas/eac-cpf/eac.rng
 
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/cpf.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/eac-cpf/cpf.rng
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/eac-cpf/eac.rng
 
-java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/eac-cpf/cpf.rng ../xml-schemas/eac-cpf/cpf.xsd
+java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/eac-cpf/eac.rng ../xml-schemas/eac-cpf/eac.xsd
 
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/cpf.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/eac-cpf/cpf.xsd 
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/eac-cpf/eac.xsd
+
+# temporary.  will change this later in case we need to process the schematron file before moving.
+cp ../source/modules/schematron/shared-rules.sch ../xml-schemas/eac-cpf/schematron/eac.sch
 
 echo "All done."
